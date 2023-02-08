c0ae8fdb797577d4c95815a9731d79d03e7adac6
kcpGOAT <90441353+kcpGOAT@users.noreply.github.com>
2023-02-07 22:29:27 -0800

    Update data-viz.py

--- data-viz.py
+++ data-viz.py
@@ -3,9 +3,19 @@ import plotly.io as io
 
 io.renderers.default = "chrome"
 
-
 cases_deaths = px.scatter(covid_table,
                           x = "total_cases",
                           y = "total_deaths",
                           trendline = "ols")
 cases_deaths.show()
+
+tests_cases = px.scatter(covid_table,
+                         x = "total_tests",
+                         y = "total_cases",
+                         trendline = "ols")
+tests_cases.show()
+
+hist_cases_per_mill = px.histogram(covid_table, 
+                                   x = "total_cases_per_mill")
+hist_cases_per_mill.show()
+
