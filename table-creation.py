63cc65535e4553bcee4f549388cdd6e87e1f6561
kcpGOAT <90441353+kcpGOAT@users.noreply.github.com>
2023-02-07 22:21:01 -0800

    Update table-creation.py

--- table-creation.py
+++ table-creation.py
@@ -1,3 +1,4 @@
+import pandas as pd
 covid_table = pd.DataFrame(data = covid_table, columns = ["id", "country", "total_cases", "new_cases",
                                                           "total_deaths", "new_deaths", "total_recovered",
                                                           "new_recovered", "active_cases", "active_critical_cases", 
