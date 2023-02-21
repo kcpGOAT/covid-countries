4e37c715eb2251ac5125f174c738fab67248eb63
kcpGOAT <90441353+kcpGOAT@users.noreply.github.com>
2023-02-20 23:52:40 -0800

    Update web-scraping.py

--- web-scraping.py
+++ web-scraping.py
@@ -8,6 +8,14 @@ soup = BeautifulSoup(covid_data.text, "html.parser")
 table_soup = soup.find("table", attrs = {"id": "main_table_countries_today"})
 rows = table_soup.find_all("tr")
 
+# With piping format
+
+rows = (
+        BeautifulSoup(covid_data.text, "html.parser")
+        .find("table", attrs = {"id": "main_table_countries_today"})
+        .find_all("tr")
+    )
+
 for row in rows:
     cols = row.find_all("td")
     cols = [x.text.strip() for x in cols]
