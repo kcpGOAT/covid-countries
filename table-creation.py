f09eb90fdbbf4b989d284aa4e7376f6a4eae637c
kcpGOAT <90441353+kcpGOAT@users.noreply.github.com>
2023-03-02 17:16:21 -0800

    Update table-creation.py

--- table-creation.py
+++ table-creation.py
@@ -22,29 +22,33 @@ numeric_cols = covid_table.drop(columns = ["country", "continent"]).columns
 for i in numeric_cols:
     covid_table[i] = covid_table[i].astype(float)
     
-# Method 2: Using pandas and polars
+# Method 2: Using pandas
 
 import pandas as pd
-import polars as pl
+import re
 
-covid_table = pd.DataFrame(data = covid_table, columns = ["id", "country", "total_cases", "new_cases",
-                                                          "total_deaths", "new_deaths", "total_recovered",
-                                                          "new_recovered", "active_cases", "active_critical_cases", 
-                                                          "total_cases_per_mill", "deaths_per_mill", "total_tests",
-                                                          "tests_per_mill", "population", "continent",
-                                                          "1_case_per_X_ppl", "1_death_per_X_ppl", "1_test_per_X_ppl",
-                                                          "new_cases_per_mill", "new_deaths_per_mill", "active_cases_per_mill"])
-
-covid_df = pl.from_pandas(covid_table)
-numeric_col = covid_df.drop(columns = ["country", "continent"]).columns
+covid_df = pl.DataFrame(covid_table[1:], columns = ["id", "country", "total_cases", "new_cases",
+                                                    "total_deaths", "new_deaths", "total_recovered",
+                                                    "new_recovered", "active_cases", "active_critical_cases", 
+                                                    "total_cases_per_mill", "deaths_per_mill", "total_tests",
+                                                    "tests_per_mill", "population", "continent",
+                                                    "1_case_per_X_ppl", "1_death_per_X_ppl", "1_test_per_X_ppl",
+                                                    "new_cases_per_mill", "new_deaths_per_mill", "active_cases_per_mill"], 
+                        orient = "row")
 
 covid_df = (
-    covid_df.lazy()
+    covid_df
     .select(
         pl.when(pl.col(pl.Utf8).is_in(["N/A", ""])).then(None).otherwise(pl.col(pl.Utf8)).keep_name()
     )
-    .drop(columns = list(covid_table.filter(regex = "new|X")))
+    .drop(columns = list(filter(re.compile("new.*|.*X.*").match, covid_df.columns)))
     .drop_nulls()
-    .with_columns(pl.col(pl.Utf8).str.replace_all(",", ""))
-    .with_columns(pl.col(numeric_col).cast(pl.Float64))
+    .with_column(pl.col(pl.Utf8).str.replace_all(",", ""))
+)
+
+numeric_col = covid_df.drop(columns = ["country", "continent"]).columns
+
+covid_df = (
+    covid_df
+    .with_column(pl.col(numeric_col).cast(pl.Float64))
 )
